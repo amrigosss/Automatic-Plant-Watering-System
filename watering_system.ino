a41711b2c3e1fd991d4341b225d2f57056c35ee9
Anna Marie Rigos <amrigosss@gmail.com>
2025-09-21 00:43:30 +0800

    feat: add Arduino sketch for watering system

---
+++ watering_system.ino
@@ -0,0 +1,32 @@
+int val = 0;
+ void setup()
+ {
+ Serial.begin(9600);
+ pinMode(A0, INPUT);
+ pinMode(6, OUTPUT);
+pinMode(7, OUTPUT);
+ }
+ void loop()
+ {
+ val = analogRead(A0);
+ Serial.println(val);
+ delay(50);
+ if (val >= 700)
+ {
+ digitalWrite(6, LOW);
+ digitalWrite(7, HIGH);
+ }
+ else
+ {
+ digitalWrite(6, HIGH);
+ digitalWrite(7, LOW);
+ }
+ printOutputState();
+ }
+ void printOutputState()
+ {
+ Serial.print("Pin 6: ");
+ Serial.println(digitalRead(6) == HIGH ? "HIGH" : "LOW");
+ Serial.print("Pin 7: ");
+ Serial.println(digitalRead(7) == HIGH ? "HIGH" : "LOW");
+ }
